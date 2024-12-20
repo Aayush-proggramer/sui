a99676113b5e282c19232e416b664c719dd6b392
Aayush-proggramer <meowaayush16@gmail.com>
2024-12-20 16:32:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var name1 = "Aayush";
+document.write(name1.toUpperCase()+"<br>")
+document.write(name1.toLowerCase()+"<br>")
+document.write(name1.startsWith('A')+"<br>")
+document.write(Math.random()+"<br>")
+document.write(Math.ceil(7.6)+"<br>")
+document.write(Math.floor(7.3)+"<br>")
+
+var username2 = "Anisha"
+document.write(username2 +"<br>")
+document.write(username2.replace('nisha', 'ayush'))
